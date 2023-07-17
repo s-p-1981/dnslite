68789b2855e41579d2df8e817d545a91bd11ec59
Sebastian Pitsch <notAnEmailAdress>
2023-07-17 22:13:43 +0200

    move constants to Baseclass DNSMessage
    - move constants for bitwise operations to Baseclass DNSMessage
    - make DNSMessage abstract

--- src/main/java/eu/pitsch_devops/dnslite/dnslite_auth_server/DNSMessage.java
+++ src/main/java/eu/pitsch_devops/dnslite/dnslite_auth_server/DNSMessage.java
@@ -1,5 +1,45 @@
 package eu.pitsch_devops.dnslite.dnslite_auth_server;
 
-public class DNSMessage {
+abstract public class DNSMessage {
 
+	// constants for bitwise operations
+	static final int MAX_UNSIZED_SHORT = 0b1111_1111_1111_1111;
+	static final int MIN_UNSIZED_SHORT = 0;
+
+	// constants to set one-bit flags or to set flag, invert then and to unset flag
+	// in 3rd byte
+	static final byte QR_FLAG = (byte)0b1000_0000; // casting necessary for ints > 127. bits preserved
+	static final byte AA_FLAG= (byte)0b0000_0100;
+	static final byte TC_FLAG= (byte)0b0000_0010;
+	static final byte RD_FLAG= (byte)0b0000_0001;
+
+	// in 4th byte
+	static final byte RA_FLAG = (byte)0b1000_0000;
+	static final byte Z_FLAG = (byte)0b0100_0000;
+	static final byte AD_FLAG = (byte)0b0010_0000;
+	static final byte CD_FLAG = (byte)0b0001_0000;
+
+	// bits in 3rd byte that are not part of the opcode. use with & to unset OPCODE bits and leave others unchanged
+	static final byte OPCODE_MASK = (byte)0b1000_0111;
+	// constants to set OPCODE. set with |, (after applying mask with &)
+	static final byte OPCODE_QUERY_BITS = (byte)0b0000_0000;
+	static final byte OPCODE_IQUERY_BITS = (byte)0b0000_1000;
+	static final byte OPCODE_STATUS_BITS = (byte)0b0001_0000;
+	static final byte OPCODE_NOTIFY_BITS = (byte)0b0010_0000;
+	static final byte OPCODE_UPDATE_BITS = (byte)0b0010_1000;
+
+	// bits in 3rd byte that are not part of the opcode. use with & to unset OPCODE bits and leave others unchanged
+	static final byte RCODE_MASK = (byte)0b1111_0000; // bits in 4th byte that are not part of the rcode
+	// constants to set RCODE. set with |, (after applying mask with &)
+	static final byte RCODE_NOERR_BITS = (byte)0b0000_0000;
+	static final byte RCODE_FORMERR_BITS = (byte)0b0000_0001;
+	static final byte RCODE_SERVFAIL_BITS = (byte)0b0000_0010;
+	static final byte RCODE_NXDOMAIN_BITS = (byte)0b0000_0011;
+	static final byte RCODE_NOTIMP_BITS = (byte)0b0000_0100;
+	static final byte RCODE_REFUSED_BITS = (byte)0b0000_0101;
+	static final byte RCODE_YXDOMAIN_BITS = (byte)0b0000_0110;
+	static final byte RCODE_YXRRSET_BITS = (byte)0b0000_0111;
+	static final byte RCODE_NXRRSET_BITS = (byte)0b0000_1000;
+	static final byte RCODE_NOTAUTH_BITS = (byte)0b0000_1001;
+	static final byte RCODE_NOTZONE_BITS = (byte)0b0000_1010;
 }
--- src/main/java/eu/pitsch_devops/dnslite/dnslite_auth_server/DNSQuestion.java
+++ src/main/java/eu/pitsch_devops/dnslite/dnslite_auth_server/DNSQuestion.java
@@ -7,53 +7,6 @@ import java.util.logging.Logger;
 public class DNSQuestion extends DNSMessage {
 	
 	static final Logger log = Logger.getLogger(DNSQuestion.class.getName());
-	// constants for bitwise operations
-	private static final int MAX_UNSIZED_SHORT = 0b1111_1111_1111_1111;
-	private static final int MIN_UNSIZED_SHORT = 0;
-	
-	// constants to set one-bit flags or to set flag, invert then and to unset flag
-	// in 3rd byte
-	private static final byte QR_FLAG = (byte)0b1000_0000; // casting necessary for ints > 127. bits preserved
-	private static final byte AA_FLAG= (byte)0b0000_0100;
-	private static final byte TC_FLAG= (byte)0b0000_0010;
-	private static final byte RD_FLAG= (byte)0b0000_0001;
-	
-	static {
-		log.fine("QR_FLAG: " + QR_FLAG);
-		log.severe("QR_FLAG: " + QR_FLAG);
-		log.severe("QR_FLAG: " + (int)QR_FLAG);
-		log.severe("QR_FLAG: " + Byte.toUnsignedInt(QR_FLAG));
-	}
-	
-	// in 4th byte
-	private static final byte RA_FLAG = (byte)0b1000_0000;
-	private static final byte Z_FLAG = (byte)0b0100_0000;
-	private static final byte AD_FLAG = (byte)0b0010_0000;
-	private static final byte CD_FLAG = (byte)0b0001_0000;
-	
-	// bits in 3rd byte that are not part of the opcode. use with & to unset OPCODE bits and leave others unchanged
-	private static final byte OPCODE_MASK = (byte)0b1000_0111;  
-	// constants to set OPCODE. set with |, (after applying mask with &)
-	private static final byte OPCODE_QUERY_BITS = (byte)0b0000_0000;
-	private static final byte OPCODE_IQUERY_BITS = (byte)0b0000_1000;
-	private static final byte OPCODE_STATUS_BITS = (byte)0b0001_0000;
-	private static final byte OPCODE_NOTIFY_BITS = (byte)0b0010_0000;
-	private static final byte OPCODE_UPDATE_BITS = (byte)0b0010_1000;
-	
-	// bits in 3rd byte that are not part of the opcode. use with & to unset OPCODE bits and leave others unchanged
-	private static final byte RCODE_MASK = (byte)0b1111_0000; // bits in 4th byte that are not part of the rcode 
-	// constants to set RCODE. set with |, (after applying mask with &)
-	private static final byte RCODE_NOERR_BITS = (byte)0b0000_0000;
-	private static final byte RCODE_FORMERR_BITS = (byte)0b0000_0001;
-	private static final byte RCODE_SERVFAIL_BITS = (byte)0b0000_0010;
-	private static final byte RCODE_NXDOMAIN_BITS = (byte)0b0000_0011;
-	private static final byte RCODE_NOTIMP_BITS = (byte)0b0000_0100;
-	private static final byte RCODE_REFUSED_BITS = (byte)0b0000_0101;
-	private static final byte RCODE_YXDOMAIN_BITS = (byte)0b0000_0110;
-	private static final byte RCODE_YXRRSET_BITS = (byte)0b0000_0111;
-	private static final byte RCODE_NXRRSET_BITS = (byte)0b0000_1000;
-	private static final byte RCODE_NOTAUTH_BITS = (byte)0b0000_1001;
-	private static final byte RCODE_NOTZONE_BITS = (byte)0b0000_1010;
 	
 	
 	// first 12 bytes - Header
@@ -87,7 +40,7 @@ public class DNSQuestion extends DNSMessage {
 		}
 
 		public DNSQuestionBuilder setFieldTransactionID(int txid) {
-			if (txid > MAX_UNSIZED_SHORT)
+			if (txid > DNSMessage.MAX_UNSIZED_SHORT)
 				throw new IllegalArgumentException(
 					String.format("TransactionID too big : was {}: should be <= 65535", txid));
 			if (txid < MIN_UNSIZED_SHORT)
@@ -98,7 +51,7 @@ public class DNSQuestion extends DNSMessage {
 			return this;
 		}
 		
-		// private  - always set for any questions
+		// private  - always set for questions, unset for answers
 		private DNSQuestionBuilder setFlagQR(boolean value) {
 			if(value) question.fixedSizeHeader[2] |= QR_FLAG;
 			else question.fixedSizeHeader[2] &= ~QR_FLAG;
